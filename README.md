814f6bc07a7f36f4254a95d9dd8ebf1217f784dd
Taha <taha.hojati@gmail.com>
2018-03-09 22:42:05 -1000

    Added sounds. added soundpool

---
+++ README.md
@@ -0,0 +1,17 @@
+Alright there are serveral parts to this: 
+Getting push notifications:
+    have a service that subscribes to push notifications. 
+Take pictures
+Play noise
+Do strobe lights
+Do face recognition
+Send to Cloud. 
+
+Do I need an activity or can I just do all of this from a service. 
+right now I will do everything in activity and when I like what I have, i see if I can put it into a service. 
+ok let's start
+
+1. play sound and strobe light
+2. take a picture
+3. send the said picture to an api. 
+4. setup push notification service. 
--- app/src/main/java/io/atthack/theftdeterent/MainActivity.java
+++ app/src/main/java/io/atthack/theftdeterent/MainActivity.java
@@ -1,13 +1,45 @@
 package io.atthack.theftdeterent;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.net.rtp.AudioStream;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    SoundPool mSoundPool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSoundPool = new SoundPool.Builder()
+                .setMaxStreams(4)
+                .setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build()
+                )
+                .build();
+        mSoundPool.load(this, R.raw.bark, 100);
+        mSoundPool.load(this, R.raw.police, 100);
+        mSoundPool.load(this, R.raw.shutter, 100);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSoundPool.release();
+        mSoundPool = null;
+    }
+
+    private void playSound(){
+
+    }
+
 }
